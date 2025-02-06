14becb4b1473fb922fa3d8d6d4c8bd0d254122f7
Surya-2004 <128910350+Surya-2004@users.noreply.github.com>
2025-02-06 09:34:36 +0530

    Changes in backend

--- backend/routes/holidaysRoute.js
+++ backend/routes/holidaysRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { fetchHolidays } = require('../controllers/holidaysController');
 
 // Route for fetching holidays
-router.get('/:country', fetchHolidays);
+router.get('/holidays/:country/:year?', fetchHolidays);
 
 module.exports = router;
